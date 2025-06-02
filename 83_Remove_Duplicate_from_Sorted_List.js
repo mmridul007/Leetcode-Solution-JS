81441d850d14d4323bfb343959dde87059e52460
Mostak-Mridul <mmridul116@gmail.com>
2025-06-03 00:14:50 +0600

    83th problem solution added

---
+++ 83_Remove_Duplicate_from_Sorted_List.js
@@ -0,0 +1,13 @@
+// Problem Link: https://leetcode.com/problems/remove-duplicates-from-sorted-list/description/
+
+var deleteDuplicates = function (head) {
+  let current = head;
+  while (current && current.next) {
+    if (current.val === current.next.val) {
+      current.next = current.next.next;
+    } else {
+      current = current.next;
+    }
+  }
+  return head;
+};
